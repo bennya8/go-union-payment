4d19f9b87fe4459a699e7813a48968a84d1d5230
benny <benny_a8@live.com>
2020-04-26 17:27:54 +0800

    split bills

--- payloads/common.go
+++ payloads/common.go
@@ -31,6 +31,25 @@ const (
 	WxApiBillDownload                   = "wx_api_bill_download"
 	WxApiSettleDownload                 = "wx_api_settle_download"
 
+	// 分账API
+	// 请求单次分账 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_1&index=1
+	WxApiProfitSharing = "wx_api_profit_sharing"
+	// 请求多次分账 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_6&index=2
+	WxApiMultiProfitSharing = "wx_api_multi_profit_sharing"
+	// 查询分账结果 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_2&index=3
+	WxApiProfitSharingQuery = "wx_api_profit_sharing_query"
+	// 添加分账接收方 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_3&index=4
+	WxApiProfitSharingAddReceiver = "wx_api_profit_sharing_add_receiver"
+	// 删除分账接收方 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_4&index=5
+	WxApiProfitSharingRemoveReceiver = "wx_api_profit_sharing_remove_receiver"
+	// 完结分账 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_5&index=6
+	WxApiProfitSharingFinish = "wx_api_profit_sharing_finish"
+	// 分账回退 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_7&index=7
+	WxApiProfitSharingReturn = "wx_api_profit_sharing_return"
+	// 回退结果查询 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_8&index=8
+	WxApiProfitSharingReturnQuery ="wx_api_profit_sharing_return_query"
+	// 分账动账通知 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_9&index=9
+
 	// alipay apis
 	AliApiPayApp UnionPaymentApi = "ali_api_pay_app"
 	AliApiPayWap                 = "ali_api_pay_wap"
