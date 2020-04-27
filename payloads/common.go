6e589dfd2fb898b718167e066fa6de511e392f72
benny <benny_a8@live.com>
2020-04-27 17:07:07 +0800

    no message

--- payloads/common.go
+++ payloads/common.go
@@ -2,6 +2,7 @@ package payloads
 
 type (
 	UnionPaymentGateway string
+	UnionPaymentNotify  string
 	UnionPaymentApi     string
 )
 
@@ -14,6 +15,12 @@ const (
 	WechatGateway                     = "Wechat"
 	QpayGateway                       = "Qpay"
 
+	// notifies
+	WxNotifyPay           UnionPaymentNotify = "wx_notify_pay"            // 支付结果通知 https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=9_7&index=8
+	WxNotifyRefund                           = "wx_notify_refund"         // 退款结果通知 https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=9_16&index=10
+	WxNotifyProfitSharing                    = "wx_notify_profit_sharing" // 分账动账通知 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_9&index=9
+
+	// apis
 	// 微信支付 API
 	WxApiPayApp       UnionPaymentApi = "wx_api_pay_app"       // 统一下单-APP支付 https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=9_1
 	WxApiPayWap                       = "wx_api_pay_wap"       // 统一下单-WAP支付 https://pay.weixin.qq.com/wiki/doc/api/H5.php?chapter=9_20&index=1
@@ -28,8 +35,6 @@ const (
 	WxApiQueryRefund                  = "wx_api_query_refund"  // 查询退款 https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=9_5
 	WxApiBillDownload                 = "wx_api_bill_download" // 下载交易账单 https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=9_6
 	WxApiFundDownload                 = "wx_api_fund_download" // 下载资金账单 https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=9_18&index=7
-	WxNotifyPay                       = "wx_notify_pay"        // 支付结果通知 https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=9_7&index=8
-	WxNotifyRefund                    = "wx_notify_refund"     // 退款结果通知 https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=9_16&index=10
 
 	// 微信支付 - 企业付款 API
 	WxApiTransfer          = "wx_api_transfer"            // 企业付款 零钱 https://pay.weixin.qq.com/wiki/doc/api/tools/mch_pay.php?chapter=14_2
@@ -46,19 +51,19 @@ const (
 	WxApiProfitSharingFinish         = "wx_api_profit_sharing_finish"          // 完结分账 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_5&index=6
 	WxApiProfitSharingReturn         = "wx_api_profit_sharing_return"          // 分账回退 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_7&index=7
 	WxApiProfitSharingReturnQuery    = "wx_api_profit_sharing_return_query"    // 回退结果查询 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_8&index=8
-	WxNotifyProfitSharing            = "wx_notify_profit_sharing"              // 分账动账通知 https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_9&index=9
 
-	// alipay apis
-	AliApiPayApp UnionPaymentApi = "ali_api_pay_app"
-	AliApiPayWap                 = "ali_api_pay_wap"
-	AliApiPayBar                 = "ali_api_pay_bar"
-	AliApiPayWeb                 = "ali_api_pay_web"
+	// 支付宝 API
+	AliApiPayApp = "ali_api_pay_app" // app支付接口2.0 https://opendocs.alipay.com/apis/api_1/alipay.trade.app.pay
+	AliApiPayWap = "ali_api_pay_wap" // 手机网站支付接口2.0 https://opendocs.alipay.com/apis/api_1/alipay.trade.wap.pay
+	AliApiPayWeb = "ali_api_pay_web" // PC网站支付接口 https://opendocs.alipay.com/apis/api_1/alipay.trade.page.pay
+	AliApiPayBar = "ali_api_pay_bar" // 统一收单交易支付接口 https://opendocs.alipay.com/apis/api_1/alipay.trade.pay
+	AliApiPayQr  = "ali_api_pay_qr"  // 统一收单线下交易预创建 https://opendocs.alipay.com/apis/api_1/alipay.trade.precreate
 
 	// QQ钱包 API
-	QpayApiPayApp = "qpay_api_pay_app" // App支付
-	QpayApiPayPub = "qpay_api_pay_pub" // 公众号支付
-	QpayApiPayQr  = "qpay_api_pay_qr"  // 扫码支付
-	QpayApiPayBar = "qpay_api_pay_bar" // 付款码支付
+	QpayApiPayApp = "qpay_api_pay_app" // 统一下单-App支付 https://qpay.qq.com/buss/wiki/38/1203
+	QpayApiPayPub = "qpay_api_pay_pub" // 统一下单-公众号支付 https://qpay.qq.com/buss/wiki/38/1203
+	QpayApiPayQr  = "qpay_api_pay_qr"  // 统一下单-扫码支付 https://qpay.qq.com/buss/wiki/38/1203
+	QpayApiPayBar = "qpay_api_pay_bar" // 付款码支付 https://qpay.qq.com/buss/wiki/1/1122
 )
 
 type IGatewayResponse interface {
